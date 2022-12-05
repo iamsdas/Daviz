86cb80591ed45ec7b3b2290553c30ec649db82ea
Hardik Agarwal <hardikagarwal@Hardiks-Dev.local>
2022-12-05 12:20:33 +0530

    fontend beautification

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -29,39 +29,6 @@ ChartJS.register(
 Chart.register(zoomPlugin);
 
 export default function Homepage() {
-  // const Data = [
-  //   {
-  //     id: 1,
-  //     year: 2016,
-  //     userGain: 80000,
-  //     userLost: 823,
-  //   },
-  //   {
-  //     id: 2,
-  //     year: 2017,
-  //     userGain: 45677,
-  //     userLost: 345,
-  //   },
-  //   {
-  //     id: 3,
-  //     year: 2018,
-  //     userGain: 78888,
-  //     userLost: 555,
-  //   },
-  //   {
-  //     id: 4,
-  //     year: 2019,
-  //     userGain: 90000,
-  //     userLost: 4555,
-  //   },
-  //   {
-  //     id: 5,
-  //     year: 2020,
-  //     userGain: 4300,
-  //     userLost: 234,
-  //   },
-  // ];
-
   const options = {
     Composition: ["Pie chart", "Donought chart", "Stacked bar chart"],
     Distribution: ["Bar chart (Histogram)", "Line chart (Area)"],
@@ -69,8 +36,7 @@ export default function Homepage() {
     Comparision: ["Line chart", "Scatter plot"],
   };
 
-  const [xAxis, setXAxis] = useState("");
-  const [yAxis, setYAxis] = useState<any>(null);
+  const [state, setState] = useState(0);
   const [response, setResponse] = useState();
   const [chartType, setChartType] = useState("");
   const [userChartType, setUserChartType] = useState("");
@@ -124,12 +90,15 @@ export default function Homepage() {
     label: option,
   }));
 
-  console.log(headerKeys);
+  const [xAxis, setXAxis] = useState("");
+  const [yAxis, setYAxis] = useState<any>(null);
 
-  var formData = new FormData();
+  console.log(headerKeys);
 
   const SendData = async (e) => {
     e.preventDefault();
+
+    var formData = new FormData();
 
     formData.append("csv_file", file);
     formData.append("data_type", userPurpose);
@@ -158,162 +127,195 @@ export default function Homepage() {
     }
   };
 
-  return (
-    <div className=" w-full h-full px-8 py-5">
-      <h1 className=" text-center font-semibold text-2xl">DaViz</h1>
-      <div className=" w-1/2 text-center mx-auto ">
-        <form>
-          <input
-            type={"file"}
-            id={"csvFileInput"}
-            accept={".csv"}
-            onChange={handleOnChange}
-          />
-
-          <button
-            className=" bg-green-400 hover:brightness-105 px-1 py-2 rounded-md my-2"
-            onClick={(e) => {
-              handleOnSubmit(e);
-            }}
-          >
-            IMPORT CSV
-          </button>
-          <br />
-
-          <br />
-          <label className="block mb-2 text-sm text-gray-900 dark:text-white font-semibold text-left">
-            *Choose the purpose of Vizualization
-          </label>
-          <select
-            onChange={(e) => setUserPurpose(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          >
-            <option value="comparision">Comparision</option>
-            <option value="distribution">Distribution</option>
-            <option value="composition">Composition</option>
-            <option value="trends">Trends</option>
-          </select>
-
-          <br />
-          <label className="block mb-2 text-sm text-gray-900 dark:text-white font-semibold text-left">
-            *Choose the options from chart types
-          </label>
-          <select
-            onChange={(e) => setUserChartType(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          >
-            {
-              {
-                composition: options.Composition.map((item, index) => {
-                  return (
-                    <option value={item} key={index}>
-                      {item}
-                    </option>
-                  );
-                }),
-                distribution: options.Distribution.map((item, index) => {
-                  return (
-                    <option value={item} key={index}>
-                      {item}
-                    </option>
-                  );
-                }),
-                comparision: options.Comparision.map((item, index) => {
-                  return (
-                    <option value={item} key={index}>
-                      {item}
-                    </option>
-                  );
-                }),
-                trends: options.Trends.map((item, index) => {
-                  return (
-                    <option value={item} key={index}>
-                      {item}
-                    </option>
-                  );
-                }),
-              }[userPurpose]
-            }
-          </select>
-
-          <br />
-          <label className="block mb-2 text-sm text-gray-900 dark:text-white font-semibold text-left">
-            * Choose the x-axis
-          </label>
-          <select
-            onChange={(e) => setXAxis(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          >
-            {headerKeys.map((item, index) => (
-              <option key={index} value={item}>
-                {item}
-              </option>
+  const userTable = (
+    <div className=" w-1/2 text-center flex flex-col justify-around mr-5">
+      <h1 className=" font-semibold">Table</h1>
+      <table className=" border-2 border-solid text-center px-2 ">
+        <thead className=" border-2 border-solid text-center px-2 ">
+          <tr key={"header"}>
+            {headerKeys.map((key) => (
+              <th className=" border-2 border-solid text-center px-2 ">
+                {key}
+              </th>
             ))}
-          </select>
+          </tr>
+        </thead>
 
-          <br />
+        <tbody className=" border-2 border-solid text-center px-2 ">
+          {array.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((val) => (
+                <td className=" border-2 border-solid text-center px-2 ">
+                  {val}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 
-          <Select
-            options={yAxisOptions}
-            onChange={(e) =>
-              setYAxis(Array.isArray(e) ? e.map((hotel) => hotel.label) : [])
-            }
-            isMulti
-          />
-
-          <button
-            className=" bg-green-400 hover:brightness-105 px-1 py-2 rounded-md my-2"
-            onClick={(e) => SendData(e)}
-          >
-            Send
+  const userChart = (
+    <div className=" w-10/12">
+      <div className=" flex flex-row justify-between">
+        <div>
+          <h1 className=" font-semibold">{userPurpose}</h1>
+        </div>
+        <div>
+          <button className=" bg-green-400 hover:brightness-105 px-1 py-2 rounded-md my-2 text-xs">
+            reset
           </button>
-        </form>
+        </div>
+      </div>
+      <Linechart chartData={chartData} />
+    </div>
+  );
+
+  const Component = [
+    {
+      name: "Table",
+      value: userTable,
+    },
+    {
+      name: "Chart",
+      value: userChart,
+    },
+  ];
+
+  return (
+    <div className=" w-full h-full px-8 py-5 flex flex-row">
+      <div className=" w-4/12 bg-slate-50 p-2 rounded-md h-[780px]">
+        <h1 className=" text-center font-semibold text-2xl">DaViz</h1>
+        <div className=" text-center mx-auto my-16 ">
+          <form>
+            <input
+              type={"file"}
+              id={"csvFileInput"}
+              accept={".csv"}
+              onChange={handleOnChange}
+            />
+
+            <button
+              className=" bg-green-400 hover:brightness-105 px-2 py-1 font-mono font-semibold text-xs rounded-md hover:scale-105"
+              onClick={(e) => {
+                handleOnSubmit(e);
+              }}
+            >
+              IMPORT CSV
+            </button>
+            <br />
+
+            <br />
+            <label className="block mb-2 pt-2 text-sm text-gray-900 dark:text-white font-semibold text-left">
+              *Choose the purpose of Vizualization
+            </label>
+            <select
+              onChange={(e) => setUserPurpose(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option value="comparision">Comparision</option>
+              <option value="distribution">Distribution</option>
+              <option value="composition">Composition</option>
+              <option value="trends">Trends</option>
+            </select>
+
+            <br />
+            <label className="block mb-2 pt-2 text-sm text-gray-900 dark:text-white font-semibold text-left">
+              *Choose the options from chart types
+            </label>
+            <select
+              onChange={(e) => setUserChartType(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              {
+                {
+                  composition: options.Composition.map((item, index) => {
+                    return (
+                      <option value={item} key={index}>
+                        {item}
+                      </option>
+                    );
+                  }),
+                  distribution: options.Distribution.map((item, index) => {
+                    return (
+                      <option value={item} key={index}>
+                        {item}
+                      </option>
+                    );
+                  }),
+                  comparision: options.Comparision.map((item, index) => {
+                    return (
+                      <option value={item} key={index}>
+                        {item}
+                      </option>
+                    );
+                  }),
+                  trends: options.Trends.map((item, index) => {
+                    return (
+                      <option value={item} key={index}>
+                        {item}
+                      </option>
+                    );
+                  }),
+                }[userPurpose]
+              }
+            </select>
+
+            <br />
+            <label className="block mb-2 pt-2 text-sm text-gray-900 dark:text-white font-semibold text-left">
+              * Choose the x-axis
+            </label>
+            <select
+              onChange={(e) => setXAxis(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              {headerKeys.map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+
+            <br />
+            <label className="block mb-2 pt-2 text-sm text-gray-900 dark:text-white font-semibold text-left">
+              * Choose the y-axis
+            </label>
+            <Select
+              options={yAxisOptions}
+              onChange={(e) =>
+                setYAxis(Array.isArray(e) ? e.map((hotel) => hotel.label) : [])
+              }
+              isMulti
+            />
+
+            <button
+              className=" bg-green-400 hover:brightness-105 my-8 py-1 px-2 font-mono font-semibold text-sm rounded-md hover:scale-105"
+              onClick={(e) => SendData(e)}
+            >
+              Send
+            </button>
+          </form>
+        </div>
       </div>
 
-      <br />
-      <div className=" flex flex-row justify-between mx-auto ">
-        <div className=" w-1/2 text-center flex flex-col justify-around mr-5">
-          <h1>Table</h1>
-          <table className=" border-2 border-solid text-center px-2 ">
-            <thead className=" border-2 border-solid text-center px-2 ">
-              <tr key={"header"}>
-                {headerKeys.map((key) => (
-                  <th className=" border-2 border-solid text-center px-2 ">
-                    {key}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-
-            <tbody className=" border-2 border-solid text-center px-2 ">
-              {array.map((item) => (
-                <tr key={item.id}>
-                  {Object.values(item).map((val) => (
-                    <td className=" border-2 border-solid text-center px-2 ">
-                      {val}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+      <div className=" w-8/12 flex flex-col h-full mx-auto items-start">
+        <div className="flex flex-row justify-around flex-nowrap bg-grey w-1/3 mx-auto border-2 border-solid h-14 bg-slate-50 ">
+          {Component.map((value, index) => {
+            return (
+              <div key={index}>
+                <button
+                  className="px-2 py-1 hover:brightness-105 bg-slate-50 hover:bg-slate-50 hover:border-2 hover:rounded-lg rounded-sm m-2 font-semibold hover:scale-105"
+                  onClick={() => setState(index)}
+                >
+                  {value.name}
+                </button>
+              </div>
+            );
+          })}
         </div>
-
-        <br />
-
-        <div className=" w-1/2 ml-5">
-          <div className=" flex flex-row justify-between">
-            <div>
-              <h1>Chart</h1>
-            </div>
-            <div>
-              <button className=" bg-green-400 hover:brightness-105 px-1 py-2 rounded-md my-2 text-xs">
-                reset
-              </button>
-            </div>
-          </div>
-
-          <Linechart chartData={chartData} />
+        <div className=" w-full py-5 flex justify-around">
+          {Component[state].value}
         </div>
       </div>
     </div>
