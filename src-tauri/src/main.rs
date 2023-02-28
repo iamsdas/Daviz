ad680041cebb0e5cb29b7623c70e573ea6d866e1
Suryashankar Das <suryashankardas.2002@gmail.com>
2023-03-01 02:42:54 +0530

    add initial rust function to load chart data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,9 +14,65 @@ fn get_columns(file_name: String) -> Vec<String> {
     return df.first().collect().unwrap().get_column_names_owned();
 }
 
+#[tauri::command]
+fn get_data_for_chart(
+    file_name: String,
+    y_axis: String,
+    x_axis: String,
+    group_by: Option<String>,
+) -> String {
+    let lazy_df = LazyCsvReader::new(file_name)
+        .has_header(true)
+        .finish()
+        .unwrap()
+        .drop_nulls(None)
+        .unique(Some(vec![x_axis.clone()]), UniqueKeepStrategy::First);
+
+    let mut df: DataFrame;
+
+    if let Some(group_by) = group_by {
+        let unique_rows_df = lazy_df
+            .clone()
+            .select([col(&group_by)])
+            .unique(None, UniqueKeepStrategy::First)
+            .collect()
+            .unwrap();
+
+        let groups = &unique_rows_df[group_by.as_str()]
+            .utf8()
+            .unwrap()
+            .into_iter()
+            .map(|x| x.unwrap().to_string())
+            .collect::<Vec<String>>();
+
+        df = lazy_df
+            .clone()
+            .select([col(&x_axis).alias("x_axis")])
+            .collect()
+            .unwrap();
+
+        for group in groups {
+            let group_df = lazy_df
+                .clone()
+                .filter(col(&group_by).eq(lit(group.as_str())))
+                .select([col(&x_axis).alias("x_axis"), col(&y_axis).alias(group)])
+                .collect()
+                .unwrap();
+            df = df.left_join(&group_df, ["x_axis"], ["x_axis"]).unwrap();
+        }
+    } else {
+        df = lazy_df
+            .select([col(&x_axis).alias("x_axis"), col(&y_axis).alias("y_axis")])
+            .collect()
+            .unwrap();
+    }
+
+    return serde_json::to_string(&df).unwrap();
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_columns])
+        .invoke_handler(tauri::generate_handler![get_data_for_chart, get_columns])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
